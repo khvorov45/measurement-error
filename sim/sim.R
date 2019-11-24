df4da272d430b40b79e938a729d4cd5ff9d8cf18
Arseniy Khvorov <khvorov45@gmail.com>
2019-11-25 07:34:59 +1100

    renamed simulation.R

--- sim/sim.R
+++ sim/sim.R
@@ -56,26 +56,7 @@ sim_data_dict <- function(fun, data_name, dict,
   do.call(fun, c(pars, seed = seed))
 }
 
-plot_scatter <- function(dat) {
-  dat %>%
-    ggplot(aes(x_meas, y_meas)) +
-    dark_theme_bw(verbose = FALSE) +
-    geom_point()
-}
 
-plot_x_error <- function(lin) {
-  lin %>%
-    ggplot(aes(x_meas, x_true)) +
-    dark_theme_bw(verbose = FALSE) +
-    geom_point()
-}
-
-plot_y_error <- function(lin) {
-  lin %>%
-    ggplot(aes(y_meas, y_true)) +
-    dark_theme_bw(verbose = FALSE) +
-    geom_point()
-}
 
 fit_stan_model <- function(model_compiled, data, iter = 2000,
                            seed = sample.int(.Machine$integer.max, 1)) {
